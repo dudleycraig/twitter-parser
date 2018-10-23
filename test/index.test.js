a093dd3f17de4eeaaaf009883cd390c843ca8d57
Dudley <dudley@node.home>
2018-10-23 21:45:45 +0000

    rough working commit

--- test/index.test.js
+++ test/index.test.js
@@ -1,3 +1,8 @@
+
+
+const userPath = 'assets/user.txt';
+const tweetPath = 'assets/tweet.txt';
+
 test('application execution', () => {
   const args = '';
   expect(require('../src/')({_: [], user:'assets/user.txt', tweet:'assets/tweet.txt' })).toBe();
